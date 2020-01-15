0dd411e935c6c452a67d434708507422c2fced81
Jess Castro <gzecastro@gmail.com>
2020-01-15 14:28:56 +0800

    Update validation testing for commission transaction

--- tests/Service/CommisionTest.php
+++ tests/Service/CommisionTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace JACastro\CommissionTask\Tests\Service;
 
+use JACastro\CommissionTask\Abstracts\TransactionInterface;
 use JACastro\CommissionTask\Entities\Transaction;
 use JACastro\CommissionTask\Service\CommissionCalculator;
 use JACastro\CommissionTask\Service\Math;
@@ -18,7 +19,7 @@ class CommissionTest extends TestCase
      *
      * @dataProvider dataProviderForCommissionTesting
      */
-    public function testCommission(Transaction $transaction, string $expectation)
+    public function testCommission(TransactionInterface $transaction, string $expectation)
     {
         $commission = new CommissionCalculator($transaction, new Math(4));
 
@@ -30,19 +31,18 @@ class CommissionTest extends TestCase
 
     public function dataProviderForCommissionTesting(): array
     {
-        $validator = new TransactionValidator();
         $math = new Math(4);
 
         return [
             'get cash in commission' => [
-                new Transaction(['2016-01-04', 4, 'natural', 'cash_in', '200', 'EUR'], $validator, $math),
+                new Transaction(['2016-01-04', 4, 'natural', 'cash_in', '200', 'EUR'], $math),
                 '0.06'
             ],
-            'get cash in commission with more then 5 EUR' => [new Transaction(['2016-01-04', 4, 'natural', 'cash_in', '1000000', 'EUR'], $validator, $math), '5.00'],
-            'get cash in commission in USD' => [new Transaction(['2016-01-04', 4, 'natural', 'cash_in', '200', 'USD'], $validator, $math), '0.06'],
-            'get natural user cash out commission' => [new Transaction(['2016-01-01', 1, 'natural', 'cash_out', '1200', 'EUR'], $validator, $math), '3.60'],
-            'get legal user cash out commission' => [new Transaction(['2016-01-01', 1, 'legal', 'cash_out', '300', 'EUR'], $validator, $math), '0.90'],
-            'get legal user cash out commission with less than 0.50' => [new Transaction(['2016-01-01', 1, 'legal', 'cash_out', '100', 'EUR'], $validator, $math), '0.50'],
+            'get cash in commission with more then 5 EUR' => [new Transaction(['2016-01-04', 4, 'natural', 'cash_in', '1000000', 'EUR'], $math), '5.00'],
+            'get cash in commission in USD' => [new Transaction(['2016-01-04', 4, 'natural', 'cash_in', '200', 'USD'], $math), '0.06'],
+            'get natural user cash out commission' => [new Transaction(['2016-01-01', 1, 'natural', 'cash_out', '1200', 'EUR'], $math), '3.60'],
+            'get legal user cash out commission' => [new Transaction(['2016-01-01', 1, 'legal', 'cash_out', '300', 'EUR'], $math), '0.90'],
+            'get legal user cash out commission with less than 0.50' => [new Transaction(['2016-01-01', 1, 'legal', 'cash_out', '100', 'EUR'], $math), '0.50'],
         ];
     }
 }
